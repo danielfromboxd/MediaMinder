416eb7d5713bdbf8789a158e65a8f50c31203f5c
Luca Nowak <luca.n@students.opit.com>
2025-02-26 17:50:36 +0100

    Add SQL scripts for creating tables, inserting data (placeholder data for now), creating indexes and dropping tables (if necessary)

---
+++ sql/drop_tables.sql
@@ -0,0 +1,14 @@
+-- Drop Media_Genres table
+DROP TABLE IF EXISTS Media_Genres;
+
+-- Drop Genres table
+DROP TABLE IF EXISTS Genres;
+
+-- Drop User_Media table
+DROP TABLE IF EXISTS User_Media;
+
+-- Drop Media table
+DROP TABLE IF EXISTS Media;
+
+-- Drop Users table
+DROP TABLE IF EXISTS Users;
