ff253ce76569d9ec94bb97a7f9aaf0014cc5b996
Luca Nowak <luca.n@students.opit.com>
2025-03-04 19:17:17 +0100

    Overwrite repository with correct files

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as SignUp } from './SignUp';
-export { default as LogIn } from './LogIn';
-export { default as Dashboard } from './Dashboard';
-export { default as WantToWatchRead } from './WantToWatchRead';
-export { default as InProgress } from './InProgress';
